d414d7e16dbfa742af6073225ec26ffb812d0634
Fatih <113294904+fatihoz5758@users.noreply.github.com>
2024-05-28 20:20:58 +0300

    contentleri hazırladmaya çalıştım - devam

--- tuna/src/pages/Home/Home.jsx
+++ tuna/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Footer from '../../components/Footer/Footer'
 import Main from '../../components/Main/Main.jsx'
 import Slider from '../../components/slider/Slider.jsx'
 import About from '../../components/Cards/About.jsx'
+import IsGvUzm from '../../components/Cards/IsGvUzm.jsx'
 
 
 const Home = () => {
@@ -14,7 +15,8 @@ const Home = () => {
       {/* <Slider />
       <Main /> */}
       {/* <Footer /> */}
-      <About />
+      {/* <About /> */}
+      <IsGvUzm />
 
     </div>
   )
