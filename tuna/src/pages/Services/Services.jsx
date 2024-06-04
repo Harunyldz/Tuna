d38ea5b18432d024b66da8c21627cc34e6cf81e4
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-06-05 01:03:07 +0300

    hizmetlerimiz ve eğitimlerimiz sayfalarına animasyon eklendi

--- tuna/src/pages/Services/Services.jsx
+++ tuna/src/pages/Services/Services.jsx
@@ -3,29 +3,38 @@ import "./Services.css";
 import { carts } from "../../Data.js";
 import { FaArrowRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Services = () => {
-  const services = carts.filter(item => item.type === "hizmet");
+  const services = carts.filter((item) => item.type === "hizmet");
 
   return (
     <div className="services-container">
-      <div className="services">
-        {services.map((cart) => (
-          <div className="services-cart" key={cart.id}>
-            <img src={cart.image} alt={cart.title} />
-            <div className="content">
-              <h3>{cart.title}</h3>
-              <hr />
-              <p>{cart.text}</p>
+      <AnimatePresence>
+        <motion.div
+          className="services"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ duration: 1 }}
+        >
+          {services.map((cart) => (
+            <div className="services-cart" key={cart.id}>
+              <img src={cart.image} alt={cart.title} />
+              <div className="content">
+                <h3>{cart.title}</h3>
+                <hr />
+                <p>{cart.text}</p>
+              </div>
+              <Link to={cart.href} className="card-button-link">
+                <button className="card-button">
+                  İNCELE <FaArrowRight className="arrow-icon" />
+                </button>
+              </Link>
             </div>
-            <Link to={cart.href} className="card-button-link">
-              <button className="card-button">
-                İNCELE <FaArrowRight className="arrow-icon" />
-              </button>
-            </Link>
-          </div>
-        ))}
-      </div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
--- tuna/src/pages/Trainings/Trainings.jsx
+++ tuna/src/pages/Trainings/Trainings.jsx
@@ -3,28 +3,37 @@ import "./Trainings.css";
 import { carts } from "../../Data.js";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa6";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Trainings() {
   const trainings = carts.filter((item) => item.type === "egitim");
   return (
     <div className="trainings-container">
-      <div className="trainings">
-        {trainings.map((cart) => (
-          <div className="trainings-cart" key={cart.id}>
-            <img src={cart.image} alt={cart.title} />
-            <div className="content">
-              <h3>{cart.title}</h3>
-              <hr />
-              <p>{cart.text}</p>
+      <AnimatePresence>
+        <motion.div
+          className="trainings"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ duration: 1 }}
+        >
+          {trainings.map((cart) => (
+            <div className="trainings-cart" key={cart.id}>
+              <img src={cart.image} alt={cart.title} />
+              <div className="content">
+                <h3>{cart.title}</h3>
+                <hr />
+                <p>{cart.text}</p>
+              </div>
+              <Link to={cart.href} className="card-button-link">
+                <button className="card-button">
+                  İNCELE <FaArrowRight className="arrow-icon" />
+                </button>
+              </Link>
             </div>
-            <Link to={cart.href} className="card-button-link">
-              <button className="card-button">
-                İNCELE <FaArrowRight className="arrow-icon" />
-              </button>
-            </Link>
-          </div>
-        ))}
-      </div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
