31d98cce0c6ee8288415380b50c7f2e8d9c38b35
Fatih <113294904+fatihoz5758@users.noreply.github.com>
2024-06-04 23:06:44 +0300

    footer biraz ekledim

--- tuna/src/components/Footer/Footer.jsx
+++ tuna/src/components/Footer/Footer.jsx
@@ -6,67 +6,47 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react
 
 
 const Footer = () => {
-  return (
-    <div className='footer-container'>
-        <div className="footer">
-            <div className="footer-left">
-                <h2>Yüksekte Çalışma Ürünleri</h2>
-                <ul>
-                    <li>Emniyet Kemerleri</li>
-                    <li>Lanyard ve Şok Emiciler</li>
-                    <li>Karabinalar</li>
-                    <li>Geri Sarımlı Düşüş Durdurucular</li>
-                    <li>Halatlar</li>
-                    <li>Ankrajlar</li>
-                    <li>Mobil Düşüş Durdurucular</li>
-                    <li>Çantalar ve Aksesuarlar</li>
-                    <li>Tripod ve Kurtarma Vinçleri</li>
-                    <li>Makaralar</li>
-                    <li>Kasklar</li>
-                    <li>Güvenlik Ağları</li>
-                </ul>
-            </div>
-            <div className="footer-center">
-                <div className="footer-logo">
-                    <img src={logo} alt="" />
-                    <div className="logo-name">TUNA</div>
-                </div>
-                <h3>Bizi Takip Edin</h3>
-                <ul className="icons">
-                    <li><FaInstagram/></li>
-                    <li><FaFacebook /></li>
-                    <li><FaTwitter /></li>
-                    <li><FaYoutube/></li>
-                    <li><FaLinkedin /></li>
-                </ul>
-            </div>
-            <div className="footer-right">
-                <div>
-                    <h2>Yaşam Hatları</h2>
+    return (
+        <div className='footer-container'>
+            <div className="footer">
+                <div className="footer-left">
+                    <h2>Tuna İş Güvenliği Hizmetleri</h2>
                     <ul>
-                        <li>Sabit Yaşam Hatları</li>
-                        <li>Mobil Yaşam Hatları</li>
+                        <li>İş Güvenliği Uzmanı</li>
+                        <li>İşyeri Hekimi</li>
+                        <li>Diğer Sağlık Personeli (İşyeri Hemşiresi)</li>
+                        <li>Risk Değerlendirmesi</li>
+                        <li>Acil Durum Eylem Planı</li>
+                        <li>Hijyen Eğitimi</li>
+                        <li>Tatbikatlı Yangın Eğitimi</li>
+                        <li>Yüksekte Çalışma Eğitimi</li>
                     </ul>
                 </div>
-                <div>
-                    <h2>Kurumsal</h2>
-                    <ul>
-                        <li>Hakkımızda</li>
-                        <li>Referanslarımız</li>
-                        <li>Kalite Politikamız</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>İletişim</h2>
-                    <ul>
-                        <li>İletişim</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+                <div className="footer-center">
+                    <div className="footer-logo">
+                        <img src={logo} alt="" />
+                        <div className="logo-name">TUNA</div>
+                    </div>
+                    <h3>Bizi Takip Edin</h3>
+                    <ul className="icons">
+                        <li><FaInstagram /></li>
+                        <li><FaFacebook /></li>
+                        <li><FaTwitter /></li>
 
-    </div>
-  )
+                    </ul>
+                </div>
+                <div className="footer-right">
+                    <div>
+                        <h2>İletişim</h2>
+                        <ul>
+                            <li>İnönü Mah. 375. Sokak No:7 Bağcılar/İstanbul</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Footer
