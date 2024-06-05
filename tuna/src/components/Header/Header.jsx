7aa0362ba91a6de884b602253a17989d579cad17
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-06-06 00:18:15 +0300

    Swiper ile slider oluşturuldu içerikler ve css ler düzenlenecek

---
+++ tuna/src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import "./Header.css";
+import {Link} from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="slider-box">
+        <div className="box1">
+          <h1>Tuna İş Güvenliği </h1>
+          <p>
+            İş Sağlığı ve Güvenliği Birimimiz Türkiye’de 6331 sayılı İş Sağlığı
+            ve Güvenliği Kanunu’nun kabul edilmesinin ardından, T.C. Çalışma ve
+            Sosyal Hizmetler Bakanlığı tarafından iş sağlığı ve güvenliği
+            hizmetleri başlığı altında tüm hizmetleri verebilme adına 2014
+            yılında yetkilendirilmiş referanslı iş güvenliği firmasıdır.
+          </p>
+
+          <Link to="/hakkimizda">
+            <button className="sliderBtn">Devamını Oku</button>
+          </Link>
+        </div>
+        <div className="box2">
+          <h3>Firmanız için İş Güvenliği Gereksinimlerini Öğrenmek için</h3>
+          <Link to="/info">
+            <button className="box2-btn1">Göster</button>
+          </Link>
+          <hr />
+          <Link to="/info">
+            {" "}
+            <button className="box2-btn">
+              Firmanız için İş Güvenliği Gereksinimlerini Öğren
+            </button>
+          </Link>
+          <hr />
+          <button className="box2-btn">Tehlike Sınıfı - Nace kodu Öğren</button>
+          <hr />
+          <button className="box2-btn">İsg Katip Onayı Nasıl Yapılır</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tuna/src/pages/Home/Home.jsx
+++ tuna/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Home.css"
 import Slider from '../../components/slider/Slider.jsx'
+import Header from '../../components/Header/Header.jsx'
 
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider />
+      <Header/>
     </div>
   )
 }
