0eeae92304e26448a17bf82e57091fc59865494c
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-06-12 02:07:57 +0300

    butonların gölgesi değiştirildi

--- tuna/src/components/Header/Header.jsx
+++ tuna/src/components/Header/Header.jsx
@@ -1,48 +1,41 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 import HeaderMenu from "../HeaderMenu/HeaderMenu";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <AnimatePresence>
-          <motion.div
-            className="header-content"
-            initial={{ opacity: 0, x: -200 }}
-            transition={{ type:"tween", duration: 1 }}
-            whileInView={{ opacity: 1, x: 0 }}
+        <motion.div
+          className="header-content"
+          initial={{ opacity: 0, x: -200 }}
+          transition={{ type: "tween", duration: 1 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        >
+          <h1>
+            <span>TUNA</span> İş Güvenliği Hizmetleri{" "}
+          </h1>
+          <p>
+            İş Sağlığı ve Güvenliği Birimimiz Türkiye’de 6331 sayılı İş Sağlığı
+            ve Güvenliği Kanunu’nun kabul edilmesinin ardından, T.C. Çalışma ve
+            Sosyal Hizmetler Bakanlığı tarafından iş sağlığı ve güvenliği
+            hizmetleri başlığı altında tüm hizmetleri verebilme adına 2014
+            yılında yetkilendirilmiş referanslı iş güvenliği firmasıdır.
+          </p>
 
-          >
-            <h1>
-              <span>TUNA</span> İş Güvenliği Hizmetleri{" "}
-            </h1>
-            <p>
-              İş Sağlığı ve Güvenliği Birimimiz Türkiye’de 6331 sayılı İş
-              Sağlığı ve Güvenliği Kanunu’nun kabul edilmesinin ardından, T.C.
-              Çalışma ve Sosyal Hizmetler Bakanlığı tarafından iş sağlığı ve
-              güvenliği hizmetleri başlığı altında tüm hizmetleri verebilme
-              adına 2014 yılında yetkilendirilmiş referanslı iş güvenliği
-              firmasıdır.
-            </p>
-
-            <Link to="/hakkimizda">
-              <button className="header-btn">Devamını Oku</button>
-            </Link>
-          </motion.div>
-        </AnimatePresence>
-        <AnimatePresence>
-          <motion.div
-            className="header-links"
-            initial={{ opacity: 0, x: 200 }}
-            transition={{ type:"tween", duration: 1 }}
-            whileInView={{ opacity: 1, x: 0 }}
-
-          >
-            <HeaderMenu />
-          </motion.div>
-        </AnimatePresence>
+          <Link to="/hakkimizda">
+            <button className="header-btn">Devamını Oku</button>
+          </Link>
+        </motion.div>
+        <motion.div
+          className="header-links"
+          initial={{ opacity: 0, x: 200 }}
+          transition={{ type: "tween", duration: 1 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        >
+          <HeaderMenu />
+        </motion.div>
       </div>
     </header>
   );
